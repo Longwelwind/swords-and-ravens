f3c891519595dc83b4df7307d08b8898defaf728
Longwelwind <longwelwind@gmail.com>
2019-10-20 12:21:56 +0200

    Add the "Ser Davos Seaworth" House Card

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/houseCardAbilities.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/houseCardAbilities.ts
@@ -1,15 +1,22 @@
 import HouseCardAbility from "./HouseCardAbility";
 import BetterMap from "../../../../utils/BetterMap";
 import TheonGreyjoyHouseCardAbility from "./TheonGreyjoyHouseCardAbility";
+import SerDavosSeaworthHouseCardAbility from "./SerDavosSeaworthHouseCardAbility";
 
 export const theonGreyjoy = new TheonGreyjoyHouseCardAbility(
     "theon-greyjoy",
     "If you are defending an area that contains either a Stronghold or a Castle,"
     + " this card gains +1 combat strength and a sword icon."
 );
+export const serDavosSeaworth = new SerDavosSeaworthHouseCardAbility(
+    "ser-davos-seaworth",
+    "If your \"Stannis Baratheon\", House card is in your discard pile, this card"
+    + " gains +1 combat strength and a sword icon."
+);
 
 const houseCardAbilities = new BetterMap<string, HouseCardAbility>([
-    [theonGreyjoy.id, theonGreyjoy]
+    [theonGreyjoy.id, theonGreyjoy],
+    [serDavosSeaworth.id, serDavosSeaworth]
 ]);
 
 export default houseCardAbilities;
