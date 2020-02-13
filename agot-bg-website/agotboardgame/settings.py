bcf45510e777f16dd55bb5a0e12052a08c863751
Longwelwind <longwelwind@gmail.com>
2020-02-13 09:19:14 +0100

    Allow other SMTP backends on the Django app

--- agot-bg-website/agotboardgame/settings.py
+++ agot-bg-website/agotboardgame/settings.py
@@ -188,8 +188,8 @@ REST_FRAMEWORK = {
 # Mail
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = env('EMAIL_PORT')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
