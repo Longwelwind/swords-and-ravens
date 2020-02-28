a2e5b31dd16056438e5ba82a9668167f9b8ba4df
Longwelwind <longwelwind@gmail.com>
2020-02-28 14:05:53 +0100

    Add Sentry to the game server

--- agot-bg-game-server/src/server/server.ts
+++ agot-bg-game-server/src/server/server.ts
@@ -1,7 +1,15 @@
 import {Server} from "ws";
 import GlobalServer from "./GlobalServer";
 import * as dotenv from "dotenv";
+import * as Sentry from "@sentry/node";
 dotenv.config();
+
+// Setup Sentry
+if (process.env.SENTRY_DSN) {
+    const SENTRY_DSN = process.env.SENTRY_DNS;
+    
+    Sentry.init({dsn: SENTRY_DSN});
+}
 
 const wsServer = new Server({port: parseInt(process.env.PORT || "5000")});
 
