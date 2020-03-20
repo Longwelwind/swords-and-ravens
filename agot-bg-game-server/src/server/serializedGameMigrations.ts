ce8a5e54d297bb53f6d0454a5410ee76098a9b86
Longwelwind <longwelwind@gmail.com>
2020-03-20 21:10:18 +0100

    Fix migration for Westeros card images

--- agot-bg-game-server/src/server/serializedGameMigrations.ts
+++ agot-bg-game-server/src/server/serializedGameMigrations.ts
@@ -1,4 +1,4 @@
-import westerosCardImages from "../client/westerosCardImages";
+import BetterMap from "../utils/BetterMap";
 
 const serializedGameMigrations: {version: string; migrate: (serializeGamed: any) => any}[] = [
     {
@@ -6,6 +6,23 @@ const serializedGameMigrations: {version: string; migrate: (serializeGamed: any)
         migrate: (serializedGame: any) => {
             // Migration for #292
             if (serializedGame.childGameState.type == "ingame") {
+                const mappings = new BetterMap([
+                    ["feast-for-crows", 2],
+                    ["put-to-the-sword", 2],
+                    ["rains-of-autumn", 2],
+                    ["sea-of-storms", 2],
+                    ["storm-of-swords", 2],
+                    ["web-of-lies", 2],
+                    ["wildlings-attack", 2],
+                    ["game-of-thrones", 1],
+                    ["dark-wings-dark-words", 1],
+                    ["clash-of-kings", 1],
+                    ["last-days-of-summer", 0],
+                    ["mustering", 0],
+                    ["winter-is-coming", 0],
+                    ["a-throne-of-blades", 0],
+                    ["supply", 0],
+                ]);
                 // The goal is to find a deckI for each card referenced
                 // in "(westeros-card-exe)cuted". Anyone will do, as long
                 // as it exists.
@@ -14,8 +31,7 @@ const serializedGameMigrations: {version: string; migrate: (serializeGamed: any)
                 ingame.gameLogManager.logs
                     .filter((log: any) => log.data.type == "westeros-card-executed")
                     .forEach((log: any) => {
-                        log.westerosDeckI = (westerosCardImages.entries
-                            .find(([_, images]) => images.entries.some(([id, _]) => id == log.westerosCardType)) as [number, any])[0];
+                        log.westerosDeckI = mappings.get(log.westerosCardType)
                     });
             }
 
