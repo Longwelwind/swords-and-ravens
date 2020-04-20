577887ad06e787aeb103a5db3cddaba81b944c71
MrHappyAsthma <mrhappyasthma@gmail.com>
2020-04-20 02:00:55 -0700

    Add game settings flag for tides of battle.
    Eventually this will be enable-able from the game lobby.
    
    For now, this will server as a feature flag to allow piecemeal
    implementation that is off by default.

--- agot-bg-game-server/src/common/EntireGame.ts
+++ agot-bg-game-server/src/common/EntireGame.ts
@@ -20,7 +20,7 @@ export default class EntireGame extends GameState<null, LobbyGameState | IngameG
     ownerUserId: string;
     name: string;
 
-    @observable gameSettings: GameSettings = {pbem: false, setupId: "base-game", playerCount: 6};
+    @observable gameSettings: GameSettings = {pbem: false, setupId: "base-game", playerCount: 6, tidesOfBattle: false};
     onSendClientMessage: (message: ClientMessage) => void;
     onSendServerMessage: (users: User[], message: ServerMessage) => void;
     onWaitedUsers: (users: User[]) => void;
@@ -333,4 +333,5 @@ export interface GameSettings {
     pbem: boolean;
     setupId: string;
     playerCount: number;
+    tidesOfBattle: boolean;
 }
