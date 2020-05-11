371fc0a6e2929a09957412566302cdcc03ed90a0
gereon77 <gereon77@users.noreply.github.com>
2020-05-11 23:59:32 +0200

    Correctly refresh house cards after penalty of Massing on the Milkwater

--- agot-bg-game-server/src/common/ingame-game-state/westeros-game-state/wildlings-attack-game-state/massing-on-the-milkwater-wildling-victory-game-state/MassingOnTheMilkwaterWildlingVictoryGameState.ts
+++ agot-bg-game-server/src/common/ingame-game-state/westeros-game-state/wildlings-attack-game-state/massing-on-the-milkwater-wildling-victory-game-state/MassingOnTheMilkwaterWildlingVictoryGameState.ts
@@ -24,26 +24,34 @@ export default class MassingOnTheMilkwaterWildlingVictoryGameState extends Wildl
             return;
         }
 
-        const highestStrength = _.max(availableHouseCards.map(hc => hc.combatStrength));
+        if (availableHouseCards.every(hc => hc.combatStrength == availableHouseCards[0].combatStrength)) {
+            // Lowest bidder has only cards of same strength left.
+            // According to Errata v2 he can decide which one to discard first, the other one will then be discarded at last
+            // and stays discarded while the other ones will be available again, which is more a benefit than a penalty...
 
-        const cardsToDiscard = availableHouseCards.filter(hc => hc.combatStrength == highestStrength);
+            this.setChildGameState(new SelectHouseCardGameState(this)).firstStart(house, availableHouseCards);
+        } else {
+            const highestStrength = _.max(availableHouseCards.map(hc => hc.combatStrength));
 
-        cardsToDiscard.forEach(hc => hc.state = HouseCardState.USED);
+            const cardsToDiscard = availableHouseCards.filter(hc => hc.combatStrength == highestStrength);
 
-        this.parentGameState.entireGame.broadcastToClients({
-            type: "change-state-house-card",
-            houseId: house.id,
-            state: HouseCardState.USED,
-            cardIds: cardsToDiscard.map(hc => hc.id)
-        });
+            cardsToDiscard.forEach(hc => hc.state = HouseCardState.USED);
 
-        this.ingame.log({
-            type: "massing-on-the-milkwater-house-cards-removed",
-            house: this.parentGameState.lowestBidder.id,
-            houseCardsUsed: cardsToDiscard.map(hc => hc.id)
-        });
+            this.parentGameState.entireGame.broadcastToClients({
+                type: "change-state-house-card",
+                houseId: house.id,
+                state: HouseCardState.USED,
+                cardIds: cardsToDiscard.map(hc => hc.id)
+            });
 
-        this.proceedNextHouse(house);
+            this.ingame.log({
+                type: "massing-on-the-milkwater-house-cards-removed",
+                house: house.id,
+                houseCardsUsed: cardsToDiscard.map(hc => hc.id)
+            });
+
+            this.proceedNextHouse(house);
+        }
     }
 
     executeForEveryoneElse(house: House): void {
@@ -58,20 +66,73 @@ export default class MassingOnTheMilkwaterWildlingVictoryGameState extends Wildl
     }
 
     onSelectHouseCardFinish(house: House, houseCard: HouseCard): void {
-        houseCard.state = HouseCardState.USED;
+        if (house == this.parentGameState.lowestBidder) {
+            // We only come here when lowest bidder had to decide which of his lowest cards to discard first
 
-        this.parentGameState.entireGame.broadcastToClients({
-            type: "change-state-house-card",
-            houseId: house.id,
-            state: HouseCardState.USED,
-            cardIds: [houseCard.id]
-        });
+            houseCard.state = HouseCardState.USED;
+            const availableHouseCards = house.houseCards.values.filter(hc => hc.state == HouseCardState.AVAILABLE);
 
-        this.ingame.log({
-            type: "massing-on-the-milkwater-house-cards-removed",
-            house: house.id,
-            houseCardsUsed: [houseCard.id]
-        });
+            if (availableHouseCards.length > 1) {
+                // For the case we allow custom decks with e.g. three 2-cards we have to repeat SelectHouseCardGameState
+                // until there is only one card left which will be finally discarded.
+
+                this.setChildGameState(new SelectHouseCardGameState(this)).firstStart(house, availableHouseCards);
+                return;
+            } else if (availableHouseCards.length == 1) {
+                const finallyDiscarded = availableHouseCards[0];
+
+                // Create an array for logging which always puts the last discarded card at the end
+                const allDiscardedHouseCards = house.houseCards.values.filter(hc => hc != finallyDiscarded && hc.combatStrength == houseCard.combatStrength);
+                allDiscardedHouseCards.push(finallyDiscarded);
+
+                this.ingame.log({
+                    type: "massing-on-the-milkwater-house-cards-removed",
+                    house: house.id,
+                    houseCardsUsed: allDiscardedHouseCards.map(hc => hc.id)
+                });
+
+                const cardsToRefresh = house.houseCards.values.filter(hc => hc.state == HouseCardState.USED || hc.state == HouseCardState.DISCARDED);
+
+                // The last card stays discarded:
+                finallyDiscarded.state = HouseCardState.USED;
+
+                this.parentGameState.entireGame.broadcastToClients({
+                    type: "change-state-house-card",
+                    houseId: house.id,
+                    state: HouseCardState.USED,
+                    cardIds: [finallyDiscarded.id]
+                });
+
+                // But the rest of his hand has to be refreshed now:
+                cardsToRefresh.forEach(hc => {
+                    hc.state = HouseCardState.AVAILABLE;
+
+                    this.parentGameState.entireGame.broadcastToClients({
+                        type: "change-state-house-card",
+                        houseId: house.id,
+                        state: HouseCardState.AVAILABLE,
+                        cardIds: [hc.id]
+                    });
+                });
+            } else {
+                throw new Error("At least one house card should be available when lowest bidder had to select a house card to discard!");
+            }
+        } else {
+            houseCard.state = HouseCardState.USED;
+
+            this.parentGameState.entireGame.broadcastToClients({
+                type: "change-state-house-card",
+                houseId: house.id,
+                state: HouseCardState.USED,
+                cardIds: [houseCard.id]
+            });
+
+            this.ingame.log({
+                type: "massing-on-the-milkwater-house-cards-removed",
+                house: house.id,
+                houseCardsUsed: [houseCard.id]
+            });
+        }
 
         this.proceedNextHouse(house);
     }
