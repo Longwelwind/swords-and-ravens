129706d4b6e90e5bff2232b416d554d956110593
gereon77 <gereon77@users.noreply.github.com>
2020-08-03 15:55:48 +0200

    Add unit type tooltips

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/UnitType.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/UnitType.ts
@@ -3,6 +3,7 @@ import RegionKind from "./RegionKind";
 export default class UnitType {
     id: string;
     name: string;
+    description: string;
     combatStrength: number | null;
     combatStrengthOnAttackStructure: number | null;
     walksOn: RegionKind;
@@ -12,6 +13,7 @@ export default class UnitType {
     constructor(
         id: string,
         name: string,
+        description: string,
         walksOn: RegionKind,
         combatStrength: number | null,
         combatStrengthOnAttackStructure: number | null = null,
@@ -20,6 +22,7 @@ export default class UnitType {
     ) {
         this.id = id;
         this.name = name;
+        this.description = description;
         this.walksOn = walksOn;
         this.canTransport = canTransport;
         this.combatStrength = combatStrength;
