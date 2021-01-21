43f20dd4517d9ef71652d3da278248adf4b01afc
andisichert <andisichert@gmail.com>
2021-01-21 20:45:39 +0100

    PaxterRedwyne

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/PaxterRedwyneAdwdHouseCardAbility.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/PaxterRedwyneAdwdHouseCardAbility.ts
@@ -1,19 +1,20 @@
 import HouseCardAbility from "./HouseCardAbility";
-import HouseCard, { HouseCardState } from "./HouseCard";
-import House from "../House";
 import CombatGameState from "../../action-game-state/resolve-march-order-game-state/combat-game-state/CombatGameState";
+import HouseCard from "./HouseCard";
+import House from "../House";
+import Unit from "../Unit";
+import {ship} from "../unitTypes";
+import {sea} from "../../game-data-structure/regionTypes";
+
 
 export default class PaxterRedwyneAdwdHouseCardAbility extends HouseCardAbility {
+    
+    modifyUnitCombatStrength(combat: CombatGameState, house: House, _houseCard: HouseCard, _houseSide: House, affectedUnit: Unit, _support: boolean, _currentStrength: number): number {
+        if (combat.defendingRegion.type == sea && affectedUnit.allegiance == house && affectedUnit.type == ship) {
+            return 1;
+        }
 
-    modifyCombatStrength(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
-        var discardedCards = 0;
-
-        _house.houseCards.forEach((card, _) =>{
-        if (card.state == HouseCardState.USED) {
-            discardedCards += 1;
-            }
-        });
-
-        return _houseCard == _affectedHouseCard ? discardedCards : 0;
+        return 0;
     }
 }
+
