e0a46b46f13754d46b1876009b26edcc11c4353a
gereon77 <gereon77@users.noreply.github.com>
2021-03-08 12:27:14 +0100

    Fix Qyburn

--- agot-bg-game-server/src/client/game-state-panel/house-card-abilities/BeforeCombatHouseCardsAbilitiesComponent.tsx
+++ agot-bg-game-server/src/client/game-state-panel/house-card-abilities/BeforeCombatHouseCardsAbilitiesComponent.tsx
@@ -5,7 +5,7 @@ import renderChildGameState from "../../utils/renderChildGameState";
 import AeronDamphairDwDAbilityComponent from "./AeronDamphairDwDAbilityComponent";
 import QyburnAbilityGameState
     from "../../../common/ingame-game-state/action-game-state/resolve-march-order-game-state/combat-game-state/before-combat-house-card-abilities-game-state/qyburn-ability-game-state/QyburnAbilityGameState";
-import QyburnAbilityComponent from "./BronnAbilityComponent";
+import QyburnAbilityComponent from "./QyburnAbilityComponent";
 import BeforeCombatHouseCardAbilitiesGameState from "../../../common/ingame-game-state/action-game-state/resolve-march-order-game-state/combat-game-state/before-combat-house-card-abilities-game-state/BeforeCombatHouseCardAbilitiesGameState";
 import AeronDamphairDwDAbilityGameState from "../../../common/ingame-game-state/action-game-state/resolve-march-order-game-state/combat-game-state/before-combat-house-card-abilities-game-state/aeron-damphair-dwd-ability-game-state/AeronDamphairDwDAbilityGameState";
 import BronnAbilityGameState from "../../../common/ingame-game-state/action-game-state/resolve-march-order-game-state/combat-game-state/before-combat-house-card-abilities-game-state/bronn-ability-game-state/BronnAbilityGameState";
