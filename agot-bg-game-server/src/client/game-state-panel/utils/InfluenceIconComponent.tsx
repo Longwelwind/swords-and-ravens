3fa5dbb59ebe73f89c088c8d4d0cd9c28f61876f
gereon77 <gereon77@users.noreply.github.com>
2021-03-11 11:26:26 +0100

    Highlight the current dominance token holder

--- agot-bg-game-server/src/client/game-state-panel/utils/InfluenceIconComponent.tsx
+++ agot-bg-game-server/src/client/game-state-panel/utils/InfluenceIconComponent.tsx
@@ -4,24 +4,40 @@ import Tooltip from "react-bootstrap/Tooltip";
 import houseInfluenceImages from "../../houseInfluenceImages";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import House from "../../../common/ingame-game-state/game-data-structure/House";
+import IngameGameState from "../../../common/ingame-game-state/IngameGameState";
+import classNames from "classnames";
 
 interface InfluenceIconComponentProps {
     house: House;
+    ingame: IngameGameState;
+    track: House[];
 }
 
 @observer
 export default class InfluenceIconComponent extends Component<InfluenceIconComponentProps> {
+    get house(): House {
+        return this.props.house;
+    }
+
+    get track(): House[] {
+        return this.props.track;
+    }
+
+    get ingame(): IngameGameState {
+        return this.props.ingame;
+    }
+
     render(): ReactNode {
         return (
             <OverlayTrigger overlay={
                     <Tooltip id="influence-icon">
-                        <b>{this.props.house.name}</b>
+                        <b>{this.house.name}</b>
                     </Tooltip>
                 }
                 placement="bottom"
             >
-                <div className="influence-icon hover-weak-outline"
-                    style={{backgroundImage: `url(${houseInfluenceImages.get(this.props.house.id)})`}}>
+                <div className={classNames("influence-icon", {"medium-outline": this.ingame.game.getTokenHolder(this.track) == this.house})}
+                    style={{backgroundImage: `url(${houseInfluenceImages.get(this.house.id)})`}}>
                 </div>
             </OverlayTrigger>
         );
