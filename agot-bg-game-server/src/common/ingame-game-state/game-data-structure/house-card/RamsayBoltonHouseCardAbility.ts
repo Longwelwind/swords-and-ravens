35986afc56db48cfd066a83d6ff58f3044cbedec
gereon77 <gereon77@users.noreply.github.com>
2021-05-20 13:16:26 +0200

    Allow special house cards to work with drafted decks

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/RamsayBoltonHouseCardAbility.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/RamsayBoltonHouseCardAbility.ts
@@ -4,22 +4,20 @@ import House from "../House";
 import HouseCard, {HouseCardState} from "./HouseCard";
 
 export default class RamsayBoltonHouseCardAbility extends HouseCardAbility {
-    modifySwordIcons(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
-        return this.doesTrigger(_houseCard, _affectedHouseCard, _house) ? 3 : 0;
+    modifySwordIcons(_combat: CombatGameState, house: House, houseCard: HouseCard, affectedHouseCard: HouseCard): number {
+        return this.doesTrigger(houseCard, affectedHouseCard, house) ? 3 : 0;
     }
 
-    modifyCombatStrength(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
-        return this.doesTrigger(_houseCard, _affectedHouseCard, _house) ? 1 : 0;
+    modifyCombatStrength(_combat: CombatGameState, house: House, houseCard: HouseCard, affectedHouseCard: HouseCard): number {
+        return this.doesTrigger(houseCard, affectedHouseCard, house) ? 1 : 0;
     }
 
     doesTrigger(houseCard: HouseCard, affectedHouseCard: HouseCard, house: House): boolean {
-        if (!house.houseCards.has("reek")) {
-            // This should never happen as Reek will always be with Ramsay
-            // It could happen if a house is created with Ramsay but without Reek.
+        if (houseCard != affectedHouseCard) {
             return false;
         }
 
-        const reek = house.houseCards.get("reek");
-        return houseCard == affectedHouseCard && reek.state == HouseCardState.AVAILABLE;
+        const houseCardWithZeroStrength = house.houseCards.values.filter(hc => hc.combatStrength == 0);
+        return houseCardWithZeroStrength.length == 1 && houseCardWithZeroStrength[0].state == HouseCardState.AVAILABLE;
     }
 }
--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/SerDavosSeaworthHouseCardAbility.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/SerDavosSeaworthHouseCardAbility.ts
@@ -4,23 +4,20 @@ import House from "../House";
 import HouseCard, {HouseCardState} from "./HouseCard";
 
 export default class SerDavosSeaworthHouseCardAbility extends HouseCardAbility {
-    modifySwordIcons(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
-        return this.doesTrigger(_houseCard, _affectedHouseCard, _house) ? 1 : 0;
+    modifySwordIcons(_combat: CombatGameState, house: House, houseCard: HouseCard, affectedHouseCard: HouseCard): number {
+        return this.doesTrigger(houseCard, affectedHouseCard, house) ? 1 : 0;
     }
 
-    modifyCombatStrength(_combat: CombatGameState, _house: House, _houseCard: HouseCard, _affectedHouseCard: HouseCard): number {
-        return this.doesTrigger(_houseCard, _affectedHouseCard, _house) ? 1 : 0;
+    modifyCombatStrength(_combat: CombatGameState, house: House, houseCard: HouseCard, affectedHouseCard: HouseCard): number {
+        return this.doesTrigger(houseCard, affectedHouseCard, house) ? 1 : 0;
     }
 
     doesTrigger(houseCard: HouseCard, affectedHouseCard: HouseCard, house: House): boolean {
-        if (!house.houseCards.has("stannis-baratheon")) {
-            // This should never happen as Stannis Baratheon will always be with Ser Davos
-            // Seaworth. It could happen if a house is created with Davos but without Stannis.
+        if (houseCard != affectedHouseCard) {
             return false;
         }
 
-        const stannisBaratheon = house.houseCards.get("stannis-baratheon");
-
-        return houseCard == affectedHouseCard && stannisBaratheon.state == HouseCardState.USED;
+        const houseCardWithFourStrength = house.houseCards.values.filter(hc => hc.combatStrength == 4);
+        return houseCardWithFourStrength.length == 1 && houseCardWithFourStrength[0].state == HouseCardState.USED;
     }
 }
