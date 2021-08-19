3aa279fbf5b8c8b117e585e92e2ccdb99d93fc8c
gereon77 <gereon77@users.noreply.github.com>
2021-08-19 13:28:00 +0200

    Enrich the django User model with new game user settings and them to the user serializer

--- agot-bg-website/agotboardgame_main/forms.py
+++ agot-bg-website/agotboardgame_main/forms.py
@@ -12,7 +12,11 @@ class UpdateUsernameForm(ModelForm):
 class UpdateSettingsForm(ModelForm):
     class Meta:
         model = User
-        fields = ["email_notification_active"]
+        fields = ["email_notification_active", "mute_games", "use_map_scrollbar", "use_house_names_for_chat", "use_responsive_layout_on_mobile"]
         labels = {
-            "email_notification_active": "PBEM Notifications"
+            "email_notification_active": "PBEM Notifications",
+            "mute_games": "Join games in the muted state",
+            "use_house_names_for_chat": "Use house names for ingame chat",
+            "use_map_scrollbar": "Join games by using the map scrollbar",
+            "use_responsive_layout_on_mobile": "Use a responsive layout on mobile devices"
         }
