114524d4f8595404a87b5a0eda22aa6fc914411b
gereon77 <gereon77@users.noreply.github.com>
2021-09-09 17:51:09 +0200

    Show the Kings Court stars when bidding for the tracks

--- agot-bg-game-server/src/client/game-state-panel/utils/InfluenceIconComponent.tsx
+++ agot-bg-game-server/src/client/game-state-panel/utils/InfluenceIconComponent.tsx
@@ -8,7 +8,7 @@ import IngameGameState from "../../../common/ingame-game-state/IngameGameState";
 import classNames from "classnames";
 
 interface InfluenceIconComponentProps {
-    house: House;
+    house: House | null;
     ingame: IngameGameState;
     track: House[];
     name: string;
@@ -16,7 +16,7 @@ interface InfluenceIconComponentProps {
 
 @observer
 export default class InfluenceIconComponent extends Component<InfluenceIconComponentProps> {
-    get house(): House {
+    get house(): House | null {
         return this.props.house;
     }
 
@@ -34,7 +34,7 @@ export default class InfluenceIconComponent extends Component<InfluenceIconCompo
             const tokenHolder = this.ingame.game.getTokenHolder(this.track);
             isTokenHolder = tokenHolder == this.house;
 
-            if (this.props.name == "Fiefdoms") {
+            if (this.props.name == "Fiefdoms" && this.house) {
                 // Highlight vassals of token holder as well
                 const possibleCommander = this.ingame.game.vassalRelations.tryGet(this.house, null);
                 if (possibleCommander == tokenHolder) {
@@ -46,13 +46,13 @@ export default class InfluenceIconComponent extends Component<InfluenceIconCompo
         }
         return this.props.ingame.rerender >= 0 && <OverlayTrigger overlay={
                 <Tooltip id="influence-icon">
-                    <b>{this.house.name}</b>
+                    <b>{this.house?.name ?? "Unknown"}</b>
                 </Tooltip>
             }
             placement="bottom"
         >
-            <div className={classNames("influence-icon", {"medium-outline": isTokenHolder})}
-                style={{backgroundImage: `url(${houseInfluenceImages.get(this.house.id)})`}}>
+            <div className={classNames("influence-icon", {"medium-outline": isTokenHolder}, {"invisible": this.house == null})}
+                style={{backgroundImage: this.house ? `url(${houseInfluenceImages.get(this.house.id)})` : "none"}}>
             </div>
         </OverlayTrigger>;
     }
