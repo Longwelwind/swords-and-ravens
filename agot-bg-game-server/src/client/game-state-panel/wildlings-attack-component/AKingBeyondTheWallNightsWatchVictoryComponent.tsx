af824eb2cab7fc42299160658c3937dc681bb759
gereon77 <gereon77@users.noreply.github.com>
2022-01-10 17:54:12 +0100

    Improve wildling attacks and add some info to the game state panel about what to do

--- agot-bg-game-server/src/client/game-state-panel/wildlings-attack-component/AKingBeyondTheWallNightsWatchVictoryComponent.tsx
+++ agot-bg-game-server/src/client/game-state-panel/wildlings-attack-component/AKingBeyondTheWallNightsWatchVictoryComponent.tsx
@@ -7,12 +7,14 @@ import SimpleChoiceGameState from "../../../common/ingame-game-state/simple-choi
 import SimpleChoiceComponent from "../SimpleChoiceComponent";
 import AKingBeyondTheWallNightsWatchVictoryGameState
     from "../../../common/ingame-game-state/westeros-game-state/wildlings-attack-game-state/a-king-beyond-the-wall-nights-watch-victory-game-state/AKingBeyondTheWallNightsWatchVictoryGameState";
+import { Col } from "react-bootstrap";
 
 @observer
 export default class AKingBeyondTheWallNightsWatchVictoryComponent extends Component<GameStateComponentProps<AKingBeyondTheWallNightsWatchVictoryGameState>> {
     render(): ReactNode {
         return (
             <>
+                <Col xs={12} className="text-center">The highest bidder moves their token to the top of one Influence track of their choice.</Col>
                 {renderChildGameState(this.props, [
                     [SimpleChoiceGameState, SimpleChoiceComponent]
                 ])}
--- agot-bg-game-server/src/client/game-state-panel/wildlings-attack-component/MammothRidersWildlingVictoryComponent.tsx
+++ agot-bg-game-server/src/client/game-state-panel/wildlings-attack-component/MammothRidersWildlingVictoryComponent.tsx
@@ -7,12 +7,14 @@ import SelectUnitsComponent from "../SelectUnitsComponent";
 import React from "react";
 import MammothRidersWildlingVictoryGameState
     from "../../../common/ingame-game-state/westeros-game-state/wildlings-attack-game-state/mammoth-riders-wildling-victory-game-state/MammothRidersWildlingVictoryGameState";
+import { Col } from "react-bootstrap";
 
 @observer
 export default class MammothRidersWildlingVictoryComponent extends Component<GameStateComponentProps<MammothRidersWildlingVictoryGameState>> {
     render(): ReactNode {
         return (
             <>
+                <Col xs={12} className="text-center">Houses must destroy units.</Col>
                 {renderChildGameState(this.props, [
                     [SelectUnitsGameState, SelectUnitsComponent]
                 ])}
