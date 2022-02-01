a3f9c142c15611ad272b6fda08d34446185db88d
gereon77 <gereon77@users.noreply.github.com>
2022-02-01 15:00:59 +0100

    Pimp the Admin UI and allow ordering and searching of objects

--- agot-bg-website/chat/admin.py
+++ agot-bg-website/chat/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from chat.models import UserInRoom, Message, Room
 
-admin.site.register(Room)
-admin.site.register(Message)
-admin.site.register(UserInRoom)
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    search_fields = ("user__username__startswith", "room__id__startswith")
+
+""" @admin.register(UserInRoom)
+class UserInRoomAdmin(admin.ModelAdmin):
+    pass """
--- agot-bg-website/chat/models.py
+++ agot-bg-website/chat/models.py
@@ -3,7 +3,6 @@ import uuid
 from django.contrib.auth import get_user_model
 from django.db import models
 
-
 class Room(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     name = models.CharField(max_length=200)
@@ -14,12 +13,19 @@ class Room(models.Model):
     def __str__(self):
         return f"{self.name} ({self.id})"
 
+    class Meta:
+        ordering = ("-created_at", )
+
 
 class Message(models.Model):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        created_at_formmated = self.created_at.strftime("%m/%d/%y %H:%M:%S")
+        return f"{created_at_formmated}: {self.user.username}: \"{self.text}\" (Room {self.room.id}: {self.room.name})"
 
 
 class UserInRoom(models.Model):
