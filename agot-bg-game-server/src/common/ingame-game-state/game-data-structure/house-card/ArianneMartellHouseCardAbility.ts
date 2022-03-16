3a78aedbf84ad5d06ceccf328682f8f3e389363e
gereon77 <gereon77@users.noreply.github.com>
2022-03-16 01:21:06 +0100

    Adapt Arianne Martell to be reused by ASoS Arianne Martell

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/ArianneMartellHouseCardAbility.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/house-card/ArianneMartellHouseCardAbility.ts
@@ -5,8 +5,12 @@ import PostCombatGameState
     from "../../action-game-state/resolve-march-order-game-state/combat-game-state/post-combat-game-state/PostCombatGameState";
 
 export default class ArianneMartellHouseCardAbility extends HouseCardAbility {
-    doesPreventAttackingArmyFromMoving(postCombat: PostCombatGameState, house: House, _houseCard: HouseCard): boolean {
+    doesPreventAttackingArmyFromMoving(postCombat: PostCombatGameState, house: House, houseCard: HouseCard): boolean {
         if (postCombat.loser == house && postCombat.defender == house) {
+            if (houseCard.id == "arianne-martell-asos") {
+                return Math.abs(postCombat.combat.stats[0].total - postCombat.combat.stats[1].total) <= 2;
+            }
+
             return true;
         }
 
