8228b11013d0480b6ecdd0ef6b2f37f8b5fa07ed
gereon77 <gereon77@users.noreply.github.com>
2022-05-02 12:16:28 +0200

    Hide the public chat for anonymous users

--- agot-bg-website/agotboardgame_main/templatetags/games_table.py
+++ agot-bg-website/agotboardgame_main/templatetags/games_table.py
@@ -5,4 +5,4 @@ register = template.Library()
 
 @register.inclusion_tag("agotboardgame_main/components/games_table.html")
 def games_table(games, user, perms):
-    return {"games": games, "user": user, "perms": perms}
+    return {"games": games, "user": user, "perms": perms, "banned_or_on_probation": user.is_authenticated and user.is_in_one_group(["On probation", "Banned"])}
