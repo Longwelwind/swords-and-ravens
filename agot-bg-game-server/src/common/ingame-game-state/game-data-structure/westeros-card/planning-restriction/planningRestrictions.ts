a99b852d536b5d7a2e11e3ed6565e444648001aa
gereon77 <gereon77@users.noreply.github.com>
2022-08-02 20:22:20 +0200

    Fix Raid/Support+1 order may not be placed during Web of Lies and Sea of Storms

--- agot-bg-game-server/src/common/ingame-game-state/game-data-structure/westeros-card/planning-restriction/planningRestrictions.ts
+++ agot-bg-game-server/src/common/ingame-game-state/game-data-structure/westeros-card/planning-restriction/planningRestrictions.ts
@@ -6,11 +6,12 @@ import RaidOrderType from "../../order-types/RaidOrderType";
 import ConsolidatePowerOrderType from "../../order-types/ConsolidatePowerOrderType";
 import MarchOrderType from "../../order-types/MarchOrderType";
 import DefenseMusterOrderType from "../../order-types/DefenseMusterOrderType";
+import RaidSupportOrderType from "../../order-types/RaidSupportOrderType";
 
 export const noMarchPlusOneOrder = new PlanningRestriction("no-march-plus-one-order", orderType => orderType instanceof MarchOrderType && orderType.attackModifier == 1);
 export const noDefenseOrder = new PlanningRestriction("no-defense-order", orderType => orderType instanceof DefenseOrderType || orderType instanceof DefenseMusterOrderType);
-export const noSupportOrder = new PlanningRestriction("no-support-order", orderType => orderType instanceof SupportOrderType);
-export const noRaidOrder = new PlanningRestriction("no-raid-order", orderType => orderType instanceof RaidOrderType);
+export const noSupportOrder = new PlanningRestriction("no-support-order", orderType => orderType instanceof SupportOrderType || orderType instanceof RaidSupportOrderType);
+export const noRaidOrder = new PlanningRestriction("no-raid-order", orderType => orderType instanceof RaidOrderType || orderType instanceof RaidSupportOrderType);
 export const noConsolidatePowerOrder = new PlanningRestriction("no-consolidate-power-order", orderType => orderType instanceof ConsolidatePowerOrderType);
 
 const planningRestrictions = new BetterMap<string, PlanningRestriction>([
