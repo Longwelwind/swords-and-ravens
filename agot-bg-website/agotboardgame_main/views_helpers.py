d2e34cb70770e0407be500b606da529d015c22e4
gereon77 <gereon77@users.noreply.github.com>
2023-04-13 21:13:47 +0200

    Indicate unread public messages by a yellow icon

--- agot-bg-website/agotboardgame_main/views_helpers.py
+++ agot-bg-website/agotboardgame_main/views_helpers.py
@@ -25,7 +25,7 @@ def enrich_games(request, games: List[Game],
             if game.view_of_game.get("waitingForIds", None) and len(game.view_of_game.get("waitingForIds")) > 0:
                 game.inactive_user_id = game.view_of_game.get("waitingForIds")[0]
 
-        # Check whether there is an unseen message in
+        # Check whether there are unseen private messages
         if enrich_important_messages and game.player_in_game is not None\
             and len(game.player_in_game) == 1 and "important_chat_rooms" in game.player_in_game[0].data:
             # `important_chat_rooms` will contain a list of room ids that must trigger a warning
@@ -45,6 +45,27 @@ def enrich_games(request, games: List[Game],
             game.unread_messages = unread_messages
         else:
             game.unread_messages = False
+
+        # Check whether there are unseen public messages
+        if enrich_important_messages and game.player_in_game is not None\
+            and len(game.player_in_game) == 1 and "publicChatRoomId" in game.view_of_game:
+            # `important_chat_rooms` will contain a list of room ids that must trigger a warning
+            # if there are unseen messages.
+            public_room_id = game.view_of_game["publicChatRoomId"]
+
+            # This is a query inside a loop, not super good for performance,
+            # but since this only applies to games of the player, it should not impact performance that much.
+            unread_public_messages = UserInRoom.objects.filter(
+                Q(room__messages__created_at__gt=F("last_viewed_message__created_at"))
+                | Q(last_viewed_message__isnull=True),
+                user=game.player_in_game[0].user,
+                room_id=public_room_id,
+                room__messages__isnull=False
+            ).exists()
+
+            game.unread_public_messages = unread_public_messages
+        else:
+            game.unread_public_messages = False
 
     if cleanup_replacement_needed_games:
         games = [game for game in games if game.inactive_players is not None]
