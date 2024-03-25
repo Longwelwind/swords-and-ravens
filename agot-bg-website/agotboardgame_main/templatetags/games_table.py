6f34b8ad0cc61c00cfba2828dcea1be9bf07def3
gereon77 <gereon77@users.noreply.github.com>
2024-03-25 10:09:01 +0100

    Add "Join as owner" button

--- agot-bg-website/agotboardgame_main/templatetags/games_table.py
+++ agot-bg-website/agotboardgame_main/templatetags/games_table.py
@@ -4,5 +4,12 @@ register = template.Library()
 
 
 @register.inclusion_tag("agotboardgame_main/components/games_table.html")
-def games_table(games, user, perms, showJoinAs = False):
-    return {"games": games, "user": user, "perms": perms, "on_probation": user.is_authenticated and user.is_in_group("On probation"), "show_join_as": showJoinAs}
+def games_table(games, user, perms, showJoinAsWaited = False, showJoinAsOwner = False):
+    return {
+        "games": games,
+        "user": user,
+        "perms": perms,
+        "on_probation": user.is_authenticated and user.is_in_group("On probation"),
+        "show_join_as_waited": showJoinAsWaited,
+        "show_join_as_owner": showJoinAsOwner
+    }
--- agot-bg-website/agotboardgame_main/views.py
+++ agot-bg-website/agotboardgame_main/views.py
@@ -191,15 +191,15 @@ def my_games(request):
     my_games = games_query.filter((Q(state=IN_LOBBY) | Q(state=ONGOING)) & Q(user_is_in_game=1)).order_by("state", "-last_active_at")
     enrich_games(request, my_games, True, False, True)
 
-    my_created_games = games_query.filter((Q(state=IN_LOBBY) | Q(state=ONGOING)) & Q(owner=request.user)).order_by("state", "-last_active_at")
-    enrich_games(request, my_created_games, False, False, False)
+    #my_created_games = games_query.filter((Q(state=IN_LOBBY) | Q(state=ONGOING)) & Q(owner=request.user)).order_by("state", "-last_active_at")
+    #enrich_games(request, my_created_games, False, False, False)
 
     last_finished_game = Game.objects.filter(state=FINISHED).annotate(players_count=Count('players')).latest()
     public_room_id = Room.objects.get(name='public').id
 
     return render(request, "agotboardgame_main/my_games.html", {
         "my_games": my_games,
-        "my_created_games": my_created_games,
+        #"my_created_games": my_created_games,
         "open_live_games": open_live_games,
         "running_live_games": running_live_games,
         "last_finished_game": last_finished_game,
